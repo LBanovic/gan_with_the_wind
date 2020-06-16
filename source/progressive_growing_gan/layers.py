0012dd8b173b34ec5a89940a8afa7184656be850
Lbanovic <luka.banovic@cigrupa.hr>
2020-06-16 20:20:40 +0200

    res

--- source/progressive_growing_gan/layers.py
+++ source/progressive_growing_gan/layers.py
@@ -66,6 +66,7 @@ class WeightScaleDense(layers.Layer):
         kernel_shape = [flatshape, self.filters]
         self.w = self.add_weight(name='kernel', shape=kernel_shape, trainable=True,
                                  initializer='random_normal', dtype='float32')
+        import pdb; pdb.set_trace()
         self.scale = tf.constant(self.gain / np.sqrt(np.prod(kernel_shape[:-1])), dtype='float32')
 
     def call(self, inputs, **kwargs):
