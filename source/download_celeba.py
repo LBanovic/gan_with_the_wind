51867002ddc048363dff5ab2caa703a7a8792391
Lbanovic <luka.banovic@cigrupa.hr>
2020-06-16 12:59:24 +0200

    d

--- source/download_celeba.py
+++ source/download_celeba.py
@@ -5,8 +5,14 @@ import sys
 resolution = int(sys.argv[1])
 mega = Mega()
 m = mega.login('luka.banovich@gmail.com', 'd!W3P2!B2G')
-for res in range(2, resolution+1):
-    tfrecord = f'tfrecords-r0{res}.tfrecords'
+if len(sys.argv) > 2:
+    for res in range(2, resolution+1):
+        tfrecord = f'tfrecords-r0{res}.tfrecords'
+        file = m.find(tfrecord)
+        print(f'Downloading {tfrecord}')
+        m.download(file)
+else:
+    tfrecord = f'tfrecords-r0{resolution}.tfrecords'
     file = m.find(tfrecord)
     print(f'Downloading {tfrecord}')
     m.download(file)
